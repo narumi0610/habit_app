39552009b0dfba84e9f1bc071881a9259c0f2078
narumi <tarakosuziko0610@gmail.com>
2023-06-15 23:41:34 +0900

    更新ボタンにbounceアニメーションをつけた

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bounce/flutter_bounce.dart';
 import 'package:habit_app/models/habit.dart';
 import 'package:habit_app/screens/parts/continuous_days_animation.dart';
 import 'package:habit_app/screens/set_goal_screen.dart';
+import 'package:habit_app/utils/app_color.dart';
 import 'package:home_widget/home_widget.dart';
 import 'package:realm/realm.dart';
 
@@ -63,47 +65,63 @@ class HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // 端末のサイズを取得
+    final width = MediaQuery.of(context).size.width / 1.5;
+
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if (_currentHabit != null) ...[
-                Text(_currentHabit!.title,
-                    style: const TextStyle(fontSize: 24)),
-                const SizedBox(height: 8),
-                ContinuousDaysAnimation(_currentHabit!.currentState),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: _updateCurrentState,
-                  child: const Text('今日の目標達成！'),
-                ),
-                if (_praiseText != null) ...[
-                  const SizedBox(height: 16),
-                  const Text('AI Praise:', style: TextStyle(fontSize: 18)),
-                  const SizedBox(height: 8),
-                  Text(_praiseText!, style: const TextStyle(fontSize: 16)),
-                ],
-              ] else ...[
-                const Text('目標がありません。設定してください', style: TextStyle(fontSize: 24)),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => SetGoalScreen(),
-                      ),
-                    ).then((_) {
-                      setState(() {});
-                    });
-                  },
-                  child: const Text('目標を設定する'),
+      body: Center(
+        child: Bounce(
+          duration: const Duration(milliseconds: 300),
+          onPressed: () async {
+            await _updateCurrentState();
+          },
+          child: Container(
+            width: width,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: AppColor.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 16,
+                  offset: Offset(0, 8),
                 ),
               ],
-            ],
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (_currentHabit != null) ...[
+                  Text(_currentHabit!.title,
+                      style: const TextStyle(fontSize: 24)),
+                  const SizedBox(height: 8),
+                  ContinuousDaysAnimation(_currentHabit!.currentState),
+                  if (_praiseText != null) ...[
+                    const SizedBox(height: 16),
+                    const Text('AI Praise:', style: TextStyle(fontSize: 18)),
+                    const SizedBox(height: 8),
+                    Text(_praiseText!, style: const TextStyle(fontSize: 16)),
+                  ],
+                ] else ...[
+                  const Text('目標がありません。設定してください',
+                      style: TextStyle(fontSize: 24)),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SetGoalScreen(),
+                        ),
+                      ).then((_) {
+                        setState(() {});
+                      });
+                    },
+                    child: const Text('目標を設定する'),
+                  ),
+                ],
+              ],
+            ),
           ),
         ),
       ),
