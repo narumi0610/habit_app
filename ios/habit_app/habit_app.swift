4c40955e8208b7372a497e4002cbd41c005b1976
narumi <tarakosuziko0610@gmail.com>
2025-03-05 11:06:53 +0900

    TimelineProviderを使用するようにして、背景色を変更

--- ios/habit_app/habit_app.swift
+++ ios/habit_app/habit_app.swift
@@ -1,88 +1,89 @@
 import WidgetKit
 import SwiftUI
-import Intents
 
-private let appGroupID = "group.habitFlutter";
+private let appGroupID = "group.habitFlutter"
 
-struct Provider: IntentTimelineProvider {
-    // ウィジェットがデータを読み込む前に表示するプレースホルダーエントリを提供するためのメソッド
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent(), currentState: 0, habitTitle: "")
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> HabitEntry {
+        HabitEntry(date: Date(), currentState: 0, habitTitle: "プレースホルダー")
     }
 
-    // ウィジェットが表示するスナップショットエントリを提供するためのメソッド
-    // ウィジェットギャラリーでウィジェットを表示する際や、ウィジェットが初めて表示される際に使用
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let data = UserDefaults.init(suiteName:appGroupID)
-        let currentState = data?.integer(forKey:  "currentState") ?? 0
-        let habitTitle = data?.string(forKey: "habitTitle") ?? ""
-        let entry = SimpleEntry(date: Date(), configuration: configuration, currentState: currentState, habitTitle: habitTitle)
+    func getSnapshot(in context: Context, completion: @escaping (HabitEntry) -> ()) {
+        let entry: HabitEntry
+        
+        if context.isPreview {
+            entry = placeholder(in: context)
+        } else {
+            let userDefaults = UserDefaults(suiteName: appGroupID)
+            let currentState = userDefaults?.integer(forKey: "currentState") ?? 0
+            let habitTitle = userDefaults?.string(forKey: "habitTitle") ?? "習慣"
+            
+            entry = HabitEntry(
+                date: Date(), 
+                currentState: currentState, 
+                habitTitle: habitTitle
+            )
+        }
+        
         completion(entry)
     }
-    
-    // ウィジェットが表示するタイムラインエントリのシーケンスを提供するためのメソッド
-    // ウィジェットが表示するエントリのシーケンスを指定し、それらのエントリがいつ表示されるかを制御する
-    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        let data: UserDefaults? = UserDefaults.init(suiteName: appGroupID)
-        let currentState = data?.integer(forKey: "currentState") ?? 0
-        let habitTitle = data?.string(forKey: "habitTitle") ?? ""
 
-        let entry = SimpleEntry(
-            date: Date(),
-            configuration: configuration,
-            currentState: currentState,
-            habitTitle: habitTitle
-        )
-
-        let timeline = Timeline(entries: [entry], policy: .never)
-        completion(timeline)
+    func getTimeline(in context: Context, completion: @escaping (Timeline<HabitEntry>) -> ()) {
+        getSnapshot(in: context) { (entry) in
+            let timeline = Timeline(entries: [entry], policy: .atEnd)
+            completion(timeline)
+        }
     }
 }
 
-struct SimpleEntry: TimelineEntry {
+struct HabitEntry: TimelineEntry {
     let date: Date
-    let configuration: ConfigurationIntent
     let currentState: Int
     let habitTitle: String
 }
 
-// 見た目を定義するためのビュー
 struct habit_appEntryView : View {
-    var entry: Provider.Entry
+    var entry: HabitEntry
     @Environment(\.colorScheme) var colorScheme
 
     var body: some View {
-        VStack {
-            Text("\(entry.habitTitle)")
-                .font(.system(size: 12))
-                .foregroundColor(Color.green)
-            Text("\(entry.currentState)")
-                .font(.system(size: 50))
-                .foregroundColor(colorScheme == .dark ? Color.white : Color(white: 0.3))
-            Text("日")
-                .font(.system(size: 14))
-                .foregroundColor(Color.green)
+        ZStack {
+            // 背景色を設定
+            Color("WidgetBackground") 
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("\(entry.habitTitle)")
+                    .font(.system(size: 12))
+                    .foregroundColor(Color.green)
+                Text("\(entry.currentState)")
+                    .font(.system(size: 50))
+                    .foregroundColor(colorScheme == .dark ? Color.white : Color(white: 0.3))
+            }
         }
     }
 }
 
 struct habit_app: Widget {
-    //ウィジェットを識別する文字列。ユーザーが選択する識別子であり、ウィジェットが表す内容をわかりやすく説明するもの
     let kind: String = "habit_app"
 
     var body: some WidgetConfiguration {
-        // ユーザーが構成可能なプロパティをウィジェットが含む
-        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
-            habit_appEntryView(entry: entry)
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            if #available(iOS 17.0, *) {
+                habit_appEntryView(entry: entry)
+                    .containerBackground(Color("WidgetBackground"), for: .widget) //ウィジェット全体の背景色を適用
+            } else {
+                habit_appEntryView(entry: entry)
+                    .background(Color("WidgetBackground")) // iOS 16以下は `.background` を使う
+            }
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
     }
 }
 
+
 struct habit_app_Previews: PreviewProvider {
     static var previews: some View {
-        habit_appEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent(),currentState: 0, habitTitle: ""))
+        habit_appEntryView(entry: HabitEntry(date: Date(), currentState: 0, habitTitle: ""))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
