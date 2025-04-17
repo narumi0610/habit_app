1a30cbdb45089aad6237fcd6659e60e3029078ba
narumi <tarakosuziko0610@gmail.com>
2025-04-17 17:58:47 +0900

    FunctionsとAdminSDKのバージョン更新した

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,36 @@
-const functions = require('firebase-functions');
+const { onDocumentCreated } = require('firebase-functions/v2/firestore');
+const { getFirestore } = require('firebase-admin/firestore');
 const admin = require('firebase-admin');
 
-// firebase-adminの初期化
+// Admin SDK を初期化
 admin.initializeApp();
 
-exports.deleteUser = functions
-  .region('asia-northeast1') // リージョンの指定
-  .firestore.document('delete_users/{docId}') // トリガーとなるFirestoreのドキュメントパス
-  .onCreate(async (snap, context) => {
-    const deleteDocument = snap.data(); // ドキュメントデータの取得
-    const uid = deleteDocument.user_id; // 削除するユーザーのUIDを取得
+// 必要なAPIをadminから取得して使う
+const auth = admin.auth();
 
-    try {
-      // Firestoreからhabitsデータの削除
-      const habitsCollection = admin.firestore().collection('habits');
-      // uidとhabitsコレクションのuser_idフィールドが一致するドキュメント取得
-      const query = habitsCollection.where('user_id', '==', uid);
-      const querySnapshot = await query.get();
-      // バッチ作成
-      const batch = admin.firestore().batch();
-      // 一致するドキュメントを削除
-      querySnapshot.forEach(doc => {
-        batch.delete(doc.ref);
-      });
+exports.deleteUserByDoc = onDocumentCreated(
+  {
+    region: 'asia-northeast1'
+  },
+  "delete_users/{docId}", async (event) => {
+  const deleteDocument = event.data.data();
+  const uid = deleteDocument.user_id;
 
-      await batch.commit();
+  try {
+    const db = getFirestore();
+    const habitsCollection = db.collection('habits');
+    const querySnapshot = await habitsCollection.where('user_id', '==', uid).get();
 
-      // Firebase Authenticationのユーザーを削除
-      await admin.auth().deleteUser(uid);
-      console.log(`Successfully deleted user with UID: ${uid}`);
-    } catch (error) {
-      console.error(`Error deleting user with UID: ${uid}`, error);
-    }
-  });
+    const batch = db.batch();
+    querySnapshot.forEach((doc) => {
+      batch.delete(doc.ref);
+    });
+
+    await batch.commit();
+
+    await auth.deleteUser(uid);
+    console.log(`Successfully deleted user with UID: ${uid}`);
+  } catch (error) {
+    console.error(`Error deleting user with UID: ${uid}`, error);
+  }
+});
