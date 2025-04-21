3e722e0b07b616027910779e2e92aa11c813b837
narumi <tarakosuziko0610@gmail.com>
2025-04-21 12:53:13 +0900

    想定外の例外はExceptionで離脱するようにした

--- lib/model/use_cases/form_validator.dart
+++ lib/model/use_cases/form_validator.dart
@@ -1,13 +1,11 @@
 // lib/model/use_cases/form_validator.dart
 import 'package:flutter/material.dart';
-import 'package:logger/logger.dart';
 
 class FormValidator {
   static bool validateForm(BuildContext context, GlobalKey<FormState> formKey) {
     final formState = formKey.currentState;
     if (formState == null) {
-      Logger().e('フォームの状態が取得できませんでした');
-      return false;
+      throw Exception('フォームの状態が取得できませんでした');
     }
     return formState.validate();
   }
